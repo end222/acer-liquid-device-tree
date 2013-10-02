dc473a7cbf35d4815189c9320d7cd37b5461c101
Pablo <pablo@localhost.localdomain>
2013-10-02 14:03:07 +0200

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,140 @@
+# Copyright (c) 2009, Code Aurora Forum.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+#######   for use when building CyanogenMod
+USE_CAMERA_STUB := false
+#######
+ 
+# Call headers from msm-3.0: needed to build libs in hardware/qcom/display
+TARGET_SPECIFIC_HEADER_PATH := device/acer/salsa/include
+ 
+TARGET_BOOTLOADER_BOARD_NAME := salsa
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+ 
+# QSD8250
+TARGET_BOARD_PLATFORM := qsd8k
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+
+BOARD_USES_GENERIC_AUDIO := false
+ 
+# Neon stuff
+#TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_ARMV7A_BUG := true
+ARCH_ARM_HAVE_NEON := true
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+#WiFi
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WLAN_DEVICE           := bcm4329
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/BCM4325.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/BCM4325_apsta.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/etc/wifi/BCM4325.bin nvram_path=/etc/wifi/nvram.txt"
+WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+ 
+#BT
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+#Fm radio
+BOARD_HAVE_FM_RADIO := false
+
+#GPS
+BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_HARDWARE := true
+# to enable the GPS HAL
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := salsa
+# AMSS version to use for GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+TARGET_PROVIDES_LIBAUDIO := true
+
+#Qcom
+TARGET_HAVE_BYPASS      := false
+TARGET_USES_C2D_COMPOSITION := false
+TARGET_USES_GENLOCK := true
+TARGET_GRALLOC_USES_ASHMEM := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DBINDER_COMPAT -DMISSING_GRALLOC_BUFFERS -DREFRESH_RATE=60
+#COMMON_GLOBAL_CFLAGS += -DQCOM_LEGACY_OMX
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+
+#GPU (Graphics)
+BOARD_EGL_CFG := device/acer/salsa/proprietary/system/lib/egl/egl.cfg
+#BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+#BOARD_USES_LEGACY_OVERLAY := true
+USE_OPENGL_RENDERER := true
+#TARGET_NO_HW_VSYNC := true
+#TARGET_NO_HW_OVERLAY := true
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+#TARGET_USES_ION := false
+#BOARD_HAVE_HDMI_SUPPORT := false
+#COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT -DQCOM_NO_SECURE_PLAYBACK -DUSES_LEGACY_EGL
+
+#WEBkit
+TARGET_FORCE_CPU_UPLOAD := true
+ENABLE_WEBGL := true
+
+# Try to get radio working (Legacy ril) RomzesRover
+# Enable gsm and network search
+COMMON_GLOBAL_CFLAGS += -DFORCE_RILD_AS_ROOT # Not sure if this is needed
+BOARD_FORCE_RILD_AS_ROOT := true
+BOARD_USES_LEGACY_RIL := true
+ 
+#sensors
+TARGET_USES_OLD_LIBSENSORS_HAL := true
+
+#kernel
+TARGET_KERNEL_SOURCE := device/acer/salsa/kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_salsa_defconfig
+BOARD_KERNEL_CMDLINE := console=null
+BOARD_KERNEL_BASE := 0x20000000
+BOARD_KERNEL_PAGESIZE := 4096
+
+#recovery
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p1
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+#BOARD_HAS_NO_MISC_PARTITION := true
+#BOARD_USES_RECOVERY_CHARGEMODE := false
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/acer/salsa/recovery/recovery_keys.c
+#TARGET_RECOVERY_PRE_COMMAND := "touch /cache/recovery/boot;sync;"
+#TARGET_RECOVERY_INITRC := device/acer/salsa/recovery.rc
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/acer/salsa/recovery_kernel
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun"
+#twrp recovery
+TARGET_RECOVERY_INITRC := device/acer/salsa/recovery.rc
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+DEVICE_RESOLUTION := 480x800
+TW_FLASH_FROM_STORAGE := true
+
+#fs
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x00500000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00500000
+#200Mb
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x0c800000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c800000
+BOARD_FLASH_BLOCK_SIZE := 4096
+
---
+++ SalsaProprietary.mk
@@ -0,0 +1,144 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Prebuilt libraries that are needed to build open-source libraries
+##
+##
+## WARNING !!! you don't need to use vendor/acer/salsa folder !!! all prebuilts and proprierarys replaced here by RomzesRover
+##
+##
+## (1) Copy Configuration files
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/etc/vold.fstab:system/etc/vold.fstab \
+    device/acer/salsa/proprietary/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    device/acer/salsa/proprietary/system/etc/sysctl.conf:system/etc/sysctl.conf \
+    device/acer/salsa/proprietary/system/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/acer/salsa/proprietary/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf 
+#    device/acer/salsa/proprietary/GooManager.apk:system/app/GooManager.apk
+
+## (2) Keypad and Touchscreen
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/usr/keychars/a1-keypad.kcm:system/usr/keychars/a1-keypad.kcm \
+    device/acer/salsa/proprietary/system/usr/keylayout/a1-keypad.kl:system/usr/keylayout/a1-keypad.kl \
+    device/acer/salsa/proprietary/system/usr/keylayout/avr.kl:system/usr/keylayout/avr.kl \
+    device/acer/salsa/proprietary/system/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/acer/salsa/proprietary/system/usr/idc/auo-touch.idc:system/usr/idc/auo-touch.idc
+
+## (3) Ramdisk
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/root/init.salsa.rc:root/init.salsa.rc \
+    device/acer/salsa/proprietary/root/init.salsa.usb.rc:root/init.salsa.usb.rc \
+    device/acer/salsa/proprietary/root/ueventd.salsa.rc:root/ueventd.salsa.rc \
+    device/acer/salsa/proprietary/root/initlogo.rle:root/initlogo.rle
+
+## (4) Init scripts
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+    device/acer/salsa/proprietary/system/etc/init.d/03swaps:system/etc/init.d/03swaps \
+    device/acer/salsa/proprietary/system/etc/init.d/05mountext:system/etc/init.d/05mountext \
+    device/acer/salsa/proprietary/system/etc/init.d/10apps2sd-redux:system/etc/init.d/10apps2sd
+
+## (5) App2sd
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/etc/profile:system/etc/profile \
+    device/acer/salsa/proprietary/system/bin/a2sd:system/bin/a2sd \
+    device/acer/salsa/proprietary/system/bin/fix_permissions:system/bin/fix_permissions \
+    device/acer/salsa/proprietary/system/xbin/zipalign:system/xbin/zipalign
+
+## (6) Compcache
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/xbin/rzscontrol:system/xbin/rzscontrol 
+
+## (7) Andreno jb drivers
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+    device/acer/salsa/proprietary/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
+    device/acer/salsa/proprietary/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    device/acer/salsa/proprietary/system/lib/libgsl.so:system/lib/libgsl.so \
+    device/acer/salsa/proprietary/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
+    device/acer/salsa/proprietary/system/lib/libC2D2.so:system/lib/libC2D2.so \
+    device/acer/salsa/proprietary/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
+    device/acer/salsa/proprietary/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+    device/acer/salsa/proprietary/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    device/acer/salsa/proprietary/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    device/acer/salsa/proprietary/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    device/acer/salsa/proprietary/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    device/acer/salsa/proprietary/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/acer/salsa/proprietary/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw   
+
+## (8) Camera
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    device/acer/salsa/proprietary/system/lib/liboemcamera.so:obj/lib/liboemcamera.so \
+    device/acer/salsa/proprietary/system/lib/libcamera.so:system/lib/libcamera.so \
+    device/acer/salsa/proprietary/system/lib/libcamera.so:obj/lib/libcamera.so
+
+## (9) WiFi
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/etc/wifi/BCM4325.bin:system/etc/wifi/BCM4325.bin \
+    device/acer/salsa/proprietary/system/etc/wifi/BCM4325_apsta.bin:system/etc/wifi/BCM4325_apsta.bin \
+    device/acer/salsa/proprietary/system/etc/wifi/BCM4325Fac.bin:system/etc/wifi/BCM4325Fac.bin \
+    device/acer/salsa/proprietary/system/etc/wifi/nvram.txt:system/etc/wifi/nvram.txt
+
+## (10) RIL
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/lib/libril-acer-1.so:system/lib/libril-acer-1.so \
+    device/acer/salsa/proprietary/system/lib/libril-acerril-hook-oem.so:system/lib/libril-acerril-hook-oem.so
+
+## (11) Sensors
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/bin/sensorcalibutil_yamaha:system/bin/sensorcalibutil_yamaha \
+    device/acer/salsa/proprietary/system/bin/sensorserver_yamaha:system/bin/sensorserver_yamaha \
+    device/acer/salsa/proprietary/system/bin/sensorstatutil_yamaha:system/bin/sensorstatutil_yamaha \
+    device/acer/salsa/proprietary/system/lib/libms3c_yamaha.so:system/lib/libms3c_yamaha.so \
+    device/acer/salsa/proprietary/system/lib/libsensor_yamaha.so:system/lib/libsensor_yamaha.so \
+    device/acer/salsa/proprietary/system/lib/hw/sensors.salsa.so:system/lib/hw/sensors.salsa.so
+
+
+## (12) other files i don't what they need
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/etc/firmware/BCM4325.hcd:system/etc/firmware/BCM4325.hcd \
+    device/acer/salsa/proprietary/system/bin/qmuxd:system/bin/qmuxd \
+    device/acer/salsa/proprietary/system/bin/brcm_patchram_plus:system/bin/brcm_patchram_plus \
+    device/acer/salsa/proprietary/system/lib/libdiag.so:system/lib/libdiag.so \
+    device/acer/salsa/proprietary/system/lib/liboncrpc.so:system/lib/liboncrpc.so \
+    device/acer/salsa/proprietary/system/lib/libqmi.so:system/lib/libqmi.so \
+    device/acer/salsa/proprietary/system/lib/libdsm.so:system/lib/libdsm.so \
+    device/acer/salsa/proprietary/system/lib/libqueue.so:system/lib/libqueue.so \
+    device/acer/salsa/proprietary/system/lib/libdll.so:system/lib/libdll.so \
+    device/acer/salsa/proprietary/system/lib/libcm.so:system/lib/libcm.so \
+    device/acer/salsa/proprietary/system/lib/libmmgsdilib.so:system/lib/libmmgsdilib.so \
+    device/acer/salsa/proprietary/system/lib/libgsdi_exp.so:system/lib/libgsdi_exp.so \
+    device/acer/salsa/proprietary/system/lib/libgstk_exp.so:system/lib/libgstk_exp.so \
+    device/acer/salsa/proprietary/system/lib/libwms.so:system/lib/libwms.so \
+    device/acer/salsa/proprietary/system/lib/libnv.so:system/lib/libnv.so \
+    device/acer/salsa/proprietary/system/lib/libwmsts.so:system/lib/libwmsts.so \
+    device/acer/salsa/proprietary/system/lib/libpbmlib.so:system/lib/libpbmlib.so \
+    device/acer/salsa/proprietary/system/lib/libdss.so:system/lib/libdss.so \
+    device/acer/salsa/proprietary/system/lib/libauth.so:system/lib/libauth.so \
+    device/acer/salsa/proprietary/system/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
+    device/acer/salsa/proprietary/system/lib/libmmipl.so:system/lib/libmmipl.so \
+    device/acer/salsa/proprietary/system/lib/libmmparser_divxdrmlib.so:system/lib/libmmparser_divxdrmlib.so \
+    device/acer/salsa/proprietary/system/lib/libmmosal.so:system/lib/libmmosal.so \
+    device/acer/salsa/proprietary/system/lib/libmmparser.so:system/lib/libmmparser.so \
+    device/acer/salsa/proprietary/system/lib/libloc.so:system/lib/libloc.so \
+    device/acer/salsa/proprietary/system/lib/libloc.so:obj/lib/libloc.so \
+    device/acer/salsa/proprietary/system/lib/libloc-rpc.so:system/lib/libloc-rpc.so \
+    device/acer/salsa/proprietary/system/lib/libcommondefs.so:system/lib/libcommondefs.so
+
+## (13) # Configs
+# Added by RomzesRover to get work dsp manager
+PRODUCT_COPY_FILES += \
+    device/acer/salsa/proprietary/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    device/acer/salsa/proprietary/system/etc/audio_policy.conf:system/etc/audio_policy.conf
